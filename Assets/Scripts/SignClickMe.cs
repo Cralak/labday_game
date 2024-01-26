f4add8fb642fe273d5a1189363860d560476626e
Nicolas <nico.dobosz@gmail.com>
2024-01-26 09:43:20 +0100

    sign

--- Assets/Scripts/SignClickMe.cs
+++ Assets/Scripts/SignClickMe.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SignClickMe : MonoBehaviour
 {
+    bool isTouching;
+    bool isDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTouching = false;
+        isDisplay = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTouching == true && Input.GetMouseButtonDown(0))
+        {
+            isDisplay = true;
+            print("sign display");
+        }
+
+    }
+
+    void OnTriggerEnter()
+    {
+        isTouching = true;
+    }
+    
+    void OnTriggerExit()
+    {
+        isTouching = false;
     }
 }
