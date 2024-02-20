5f3d26d6578a1daa26483fafdeef8d238eeec590
Cralak <115760654+Cralak@users.noreply.github.com>
2024-02-20 16:33:12 +0100

    wordle progress

--- Assets/Scripts/wordle/wordle.cs
+++ Assets/Scripts/wordle/wordle.cs
@@ -3,14 +3,20 @@ using System.IO;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine.PlayerLoop;
+using System.Xml.Linq;
+using System;
 
 public class validWords : MonoBehaviour
 {
-    StreamReader reader = new StreamReader("");
+    StreamReader reader = new StreamReader("Assets/Scripts//wordle/validWords.txt");
+    StreamReader reader2 = new StreamReader("Assets/Scripts//wordle/wordChoice.txt");
     private static List<string> validWordsList;
+    private static List<string> wordChoice;
 
     void Start(){
-        validWordsList = new List<string>();
-    }
+        validWordsList = new List<string>((reader.ReadToEnd()).Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+        wordChoice = new List<string>((reader2.ReadToEnd()).Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
 
+        Debug.Log(wordChoice[1]);
+    }
 }
