b6e032b90f0f7589ccfbee448e30af9ea41f35f4
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-02-22 09:57:20 +0100

    indoor Tv

--- Assets/Scripts/TV.cs
+++ Assets/Scripts/TV.cs
@@ -1,13 +1,19 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TV : MonoBehaviour
 {
     [SerializeField] AudioClip TVSound;
     [SerializeField] AudioClip TVGlitch;
+    [SerializeField] float interval;
+    [SerializeField] Color lightBlue;
+    [SerializeField] Color darkBlue;
 
     AudioSource source;
     GameObject player;
     Diary diary;
+    Light screenLight;
     bool hasGlitched;
 
     // Start is called before the first frame update
@@ -16,13 +22,16 @@ public class TV : MonoBehaviour
         player = GameObject.Find("Player");
         diary = GameObject.Find("OpenedDiary").GetComponent<Diary>();
         source = GetComponent<AudioSource>();
+        screenLight = GetComponentInChildren<Light>();
         hasGlitched = false;
+
+        StartCoroutine(LightGlitch(interval));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) < 5.0f)
+        if (Vector3.Distance(player.transform.position, transform.position) < 2.0f)
         {
             source.clip = TVGlitch;
             if (!source.isPlaying) source.Play();
@@ -39,6 +48,29 @@ public class TV : MonoBehaviour
             if (!source.isPlaying) source.Play();
         }
 
-        source.volume = PlayerPrefs.GetFloat("SFX") * (10.0f - Vector3.Distance(player.transform.position, transform.position)) / 10.0f;
+        source.volume = PlayerPrefs.GetFloat("SFX") * (4.0f - Vector3.Distance(player.transform.position, transform.position)) / 4.0f;
+    }
+
+    IEnumerator LightGlitch(float interval)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+
+            for (int i = 0; i < 3; i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+
+                screenLight.color = darkBlue;
+
+                yield return new WaitForSeconds(0.1f);
+
+                screenLight.color = lightBlue;
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            screenLight.color = darkBlue;
+        }
     }
 }
