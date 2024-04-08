eab1ca92f7d4732cfb87b47df4422e385ad02bd1
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-04-08 12:29:14 +0200

    made doors supposed to be opened opened after leaving and going back to indoor

--- Assets/Scripts/Indoor/wordle/wordle.cs
+++ Assets/Scripts/Indoor/wordle/wordle.cs
@@ -38,32 +38,35 @@ public class Wordle : MonoBehaviour
 
     void Start()
     {
-        validWords = new List<string>(reader.ReadToEnd().Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
-        wordChoice = new List<string>(reader2.ReadToEnd().Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
-        reader.Close();
-        reader2.Close();
-        for (int i = 0; i < validWords.Count; i++)
+        if (KeyEvents.wordleCode != null)
         {
-            validWords[i] = validWords[i][..5];
+            validWords = new List<string>(reader.ReadToEnd().Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+            wordChoice = new List<string>(reader2.ReadToEnd().Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+            reader.Close();
+            reader2.Close();
+            for (int i = 0; i < validWords.Count; i++)
+            {
+                validWords[i] = validWords[i][..5];
+            }
+            for (int i = 0; i < wordChoice.Count; i++)
+            {
+                wordChoice[i] = wordChoice[i][..5];
+            }
+
+            flashlight = GameObject.Find("Flashlight");
+            playerCamera = flashlight.transform.parent.gameObject;
+            player = playerCamera.transform.parent.gameObject;
+            UI = GameObject.Find("UI").GetComponent<Canvas>();
+            diary = GameObject.Find("OpenedDiary").GetComponent<Diary>();
+
+            greenGuess = new Color(0.0f, 0.79f, 0.0f);
+            yellowGuess = new Color(1.0f, 0.79f, 0.0f);
+            wrongGuess = Color.gray;
+            setLettersRed = SetLettersRed();
+
+            mysteryWord = RandomWord();
+            diary.SetEventText("wordle", mysteryWord + "... Why is a word that terrifying the answer to this puzzle?");
         }
-        for (int i = 0; i < wordChoice.Count; i++)
-        {
-            wordChoice[i] = wordChoice[i][..5];
-        }
-
-        flashlight = GameObject.Find("Flashlight");
-        playerCamera = flashlight.transform.parent.gameObject;
-        player = playerCamera.transform.parent.gameObject;
-        UI = GameObject.Find("UI").GetComponent<Canvas>();
-        diary = GameObject.Find("OpenedDiary").GetComponent<Diary>();
-
-        greenGuess = new Color(0.0f, 0.79f, 0.0f);
-        yellowGuess = new Color(1.0f, 0.79f, 0.0f);
-        wrongGuess = Color.gray;
-        setLettersRed = SetLettersRed();
-
-        mysteryWord = RandomWord();
-        diary.SetEventText("wordle", mysteryWord + "... Why is a word that terrifying the answer to this puzzle?");
     }
 
     void Update()
