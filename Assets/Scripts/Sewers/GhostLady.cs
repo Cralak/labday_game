a935706b8ee0551638b82a1ffc561f71c0900e47
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-04-08 15:58:51 +0200

    added ghost lady that follows us in sewers

---
+++ Assets/Scripts/Sewers/GhostLady.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostLady : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    Transform player;
+    bool isFollowing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+        StartCoroutine(GoToStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing) navMeshAgent.destination = player.position;
+    }
+
+    IEnumerator GoToStart()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        transform.DORotate(new Vector3(0.0f, 180.0f, 0.0f), 1.0f);
+
+        yield return new WaitForSeconds(1.0f);
+
+        transform.DOMoveZ(-10.0f, 6.0f);
+
+        yield return new WaitForSeconds(5.0f);
+
+        isFollowing = true;
+    }
+}
